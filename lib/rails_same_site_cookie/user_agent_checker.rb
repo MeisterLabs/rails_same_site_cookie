d5f4ec687f9d07f2d4189bae03c4ea23455d9065
Timo Kropp <timo@meisterlabs.com>
2020-05-11 17:29:13 +0200

    Add Firefox support.

--- lib/rails_same_site_cookie/user_agent_checker.rb
+++ lib/rails_same_site_cookie/user_agent_checker.rb
@@ -17,19 +17,35 @@ module RailsSameSiteCookie
     private
 
     def same_site_supported?
-      chrome_with_support?
+      chrome_with_support? || firefox_with_support?
     end
 
     def chrome_with_support?
       chromium_based? && chromium_version_from?(67)
     end
 
+    def firefox_with_support?
+      is_firefox? && firefox_version_from?(61)
+    end
+
     def chromium_based?
       !!/Chrom(e|ium)/.match(user_agent)
     end
 
+    def is_firefox?
+      !!/Firefox/.match(user_agent)
+    end
+
     def chromium_version_from?(min_version)
-      match = /Chrom[^\/]+\/(\d+)[\.\d]*/.match(user_agent)
+      browser_version_from?(/Chrom[^\/]+/, min_version)
+    end
+
+    def firefox_version_from?(min_version)
+      browser_version_from?(/Firefox/, min_version)
+    end
+
+    def browser_version_from?(browser_regex, min_version)
+      match = /#{browser_regex}\/(\d+)[\.\d]*/.match(user_agent)
       return false unless match
 
       version = match[1].to_i
